6bb322ba86cf5ed2ba5621538402fc070e33383e
mayoimayoi <tky529@gmail.com>
2021-09-23 18:28:40 +0900

    body-parser delete

--- app.js
+++ app.js
@@ -2,13 +2,12 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
-const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const NodeMailer = require("nodemailer");
 const helmet = require("helmet");
 
 app.use(express.static(__dirname + "/public"));
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 require("dotenv").config();
 app.set("view engine", "ejs");
 app.use(helmet.xssFilter());
